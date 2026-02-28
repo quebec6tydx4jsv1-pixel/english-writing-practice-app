db6c88a075e7d8fb5af3d08c39cf07b51b22c382
quebec6tydx4jsv1-pixe <quebec6tydx4jsv1@gmail.com>
2026-02-28 18:02:30 +0900

    テーブル作成(Theme, Question)

---
+++ db/migrate/20260228051904_create_themes.rb
@@ -0,0 +1,10 @@
+class CreateThemes < ActiveRecord::Migration[8.1]
+  def change
+    create_table :themes do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_27_144517) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_28_052823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "questions", force: :cascade do |t| # AIが出題する日本語文の問題
+    t.datetime "created_at", null: false 
+    t.string "source"
+    t.text "text"
+    t.bigint "theme_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["theme_id"], name: "index_questions_on_theme_id" 
+  end
+
+  create_table "themes", force: :cascade do |t| # 出題テーマ（例：日常会話、ビジネス、旅行など）
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +41,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_27_144517) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+  
+  add_foreign_key "questions", "themes"
 end
