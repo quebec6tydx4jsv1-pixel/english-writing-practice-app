1d16e463551c7cb365d7a88c44c6ba916e0719eb
quebec6tydx4jsv1-pixe <quebec6tydx4jsv1@gmail.com>
2026-03-25 00:28:52 +0900

    プライバシポリシー等追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-  has_many :user_answers, dependent: :destroy # 英作文の回答 
+  has_many :user_answers, dependent: :destroy # 英作文の回答
   has_many :ai_corrections, through: :user_answers # AIによる添削
   has_many :mistakes, through: :ai_corrections # AIによる添削で指摘された誤り
   has_many :user_weak_expressions, dependent: :destroy # ユーザーの弱点表現
