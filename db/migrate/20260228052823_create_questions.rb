fb8a7aa66fe693c613f6cf044caa6512c1b3a9ad
quebec6tydx4jsv1-pixe <quebec6tydx4jsv1@gmail.com>
2026-03-25 01:39:19 +0900

    プライバシポリシー等追加

--- db/migrate/20260228052823_create_questions.rb
+++ db/migrate/20260228052823_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[8.1]
     create_table :questions do |t|
       t.text :input_theme # テーマ
       t.text :input_situation # テーマの状況
-      t.text :text #　問題文(日本語)
+      t.text :text # 問題文(日本語)
       t.string :source
 
       t.timestamps
