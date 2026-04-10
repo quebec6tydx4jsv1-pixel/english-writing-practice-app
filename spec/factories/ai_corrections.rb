e3946889689186c5d11eca6f81b29cfe8d8e65c0
quebec6tydx4jsv1-pixe <quebec6tydx4jsv1@gmail.com>
2026-04-10 17:22:24 +0900

    feat: add RSpec tests with FactoryBot

--- spec/factories/ai_corrections.rb
+++ spec/factories/ai_corrections.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :ai_correction do
     corrected_text { Faker::Lorem.sentence }
     score { rand(60..100) }
-    feedback_json { { comments: [Faker::Lorem.sentence] } }
+    feedback_json { { comments: [ Faker::Lorem.sentence ] } }
     association :correctable, factory: :user_answer
   end
 end
