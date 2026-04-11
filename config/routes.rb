93879a8619df72468c57ab9352eccf85761dabff
quebec6tydx4jsv1-pixe <quebec6tydx4jsv1@gmail.com>
2026-04-11 20:41:27 +0900

    feat: add Google OAuth login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get "pages/privacy"
   # get "pages/contact"
   # get "home/index"
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
 
